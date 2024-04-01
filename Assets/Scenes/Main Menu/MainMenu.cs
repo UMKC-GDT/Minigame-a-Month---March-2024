8d309a2ec15dbe75cde849fe3f4fd8f55e09884b
umkcVR <cgch6w@umsystem.edu>
2024-03-31 21:26:18 -0500

    Main menu buttons work

--- Assets/Scenes/Main Menu/MainMenu.cs
+++ Assets/Scenes/Main Menu/MainMenu.cs
@@ -5,8 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public GameObject credits;
+
     public void PlayGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+
+    public void OpenCredits(){
+        credits.SetActive(true);
+    }
+
+    public void CloseCredits(){
+        credits.SetActive(false);
+    }
+
+    public void QuitGame(){
+        Debug.Log("Exits Game");
+        Application.Quit();
     }
    
 }
